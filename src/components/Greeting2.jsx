1c4969ec1d6fac3631bb1ffe827e47209bc85510
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-11-03 16:04:21 +0100

    messing around

---
+++ src/components/Greeting2.jsx
@@ -0,0 +1,5 @@
+function Greeting2() {
+  return <h1>&quot;hiya&quot;</h1>;
+}
+
+export default Greeting2;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Greeting from "./components/Greeting.jsx";
+import Greeting2 from "./components/Greeting2.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Greeting />
+    <Greeting2 />
+  </StrictMode>
+);

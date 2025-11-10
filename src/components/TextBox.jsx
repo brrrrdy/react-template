301000dd01b8ea9c1bd9b69833ab8782359b6bf5
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-11-10 15:38:35 +0100

    eslint updated

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -1,13 +1,12 @@
 function TextBox() {
+  const animals = ["Lion", "Cow", "Snake", "Lizard", "Pig"];
+
+  const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>);
+
   return (
     <div>
-      <h1>Test title</h1>
-      <svg>
-        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
-      </svg>
-      <form>
-        <input type="text" />
-      </form>
+      <h1>Animals: </h1>
+      <ul>{animalsList}</ul>
     </div>
   );
 }

7a37fc75bc854c983904ba67e4b51bdd37965479
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-11-10 15:34:23 +0100

    jsx files exercise

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import Greeting from "./components/Greeting.jsx";
 import Greeting2 from "./components/Greeting2.jsx";
+import TextBox from "./components/TextBox.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <Greeting />
     <Greeting2 />
+    <TextBox />
   </StrictMode>
 );

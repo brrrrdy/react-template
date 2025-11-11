0e1f4d6e1d878c10271724b6dd9ba3b744c90819
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-11-11 13:58:26 +0100

    practice

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import Greeting from "./components/Greeting.jsx";
 import Greeting2 from "./components/Greeting2.jsx";
 import TextBox from "./components/TextBox.jsx";
 import AnimalsList from "./components/List.jsx";
+import PackingList from "./components/PackingList.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <Greeting />
     <Greeting2 />
+    <PackingList />
     <TextBox />
     <AnimalsList />
   </StrictMode>
